e3a51c3821bf842419049ac9717db4fe7d8980bb
Arseniy Savin <arseniy.savin@effective.band>
2024-11-26 17:50:16 +0600

    fix: added .env, readme formatting

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,23 +2,17 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Library, Language
 
-#admin.site.register(Book)
-#admin.site.register(Author)
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
 admin.site.register(Language)
 admin.site.register(Library)
 
 # Define the admin class
 
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
-
 
 # Register the Admin classes for Book using the decorator
 
